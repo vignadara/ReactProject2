289c6aa03c10bb86d0fce6345d6f895abc52df25
“vignadara” <vignatif@gmail.com>
2023-10-24 16:51:42 -0400

    add css file

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+    "h1",
+     {id: "heading"},
+    "hello react");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(heading);
